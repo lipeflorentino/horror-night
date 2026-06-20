9018ceeb65b898be9e30c2b6057ed9c868cdd9a7
lipeflima <lipeflorentino@yahoo.com>
2026-06-20 18:19:02 -0300

    feat: new

--- Scripts/CombatV2/Model/Perks/PerkEnums.cs
+++ Scripts/CombatV2/Model/Perks/PerkEnums.cs
@@ -1,31 +1,70 @@
 public enum PerkTrigger
 {
     BeforeRoll,
+    AfterRoll,
     AfterAccuracyRoll,
     PowerMultiplier,
     AfterResolve,
-    OnActionResolved  
+    OnActionResolved,
+    OnTurnStart,
+    OnTurnEnd,
+    OnTrickCast,  
+    OnInitiativeResolve,
+    OnCombatEnd,
+    OnCombatVictory,
 }
 
 public enum PerkModifierTarget
 {
     MinRollPercent,
+    MaxRollPercent,
+    MinRollValue,
+    MaxRollValue,
     ExtraDice,
     PowerMultiplier,
     DamagePercent,
     MomentumPoints,
     Focus,
     Strength,
+    Initiative,
+    DefensePercent,
+    AttackPercent,
+    Defense,
+    Attack,
+    Mind,
+    Heart,
+    Body,
+    Accuracy,
+    Agility,
+    PowerDicesCount,
+    AccuracyDicesCount,
+
 }
 
 public enum PerkConditionKey
 {
     Always,
     RollValueEquals,
+    RollValueGreaterThan,
+    RollValueLessThan,
     RollTierEquals,
     RollSumEquals,
+    RollSumGreaterThan,
+    RollSumLessThan,
     RollSumEqualsAttackersRollSum,
-    BlockedAttack
+    BlockedAttack,
+    EvadedAttack,
+    PerriedAttack,
+    CriticalHit,
+    MissedAttack,
+    HitAttack,
+    DamageDealt,
+    DamageTaken,
+    ExtraDice,
+    CombatEnd,
+    CombatVictory,
+    IncomingDamageGreaterThan,
+
 }
 
 public enum PerkOperation
